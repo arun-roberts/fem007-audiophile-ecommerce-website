37e89c0a440f7f7c1f3b14f6f33a98596fcf86a6
Arun Roberts <arunplaysbass@gmail.com>
2022-03-17 17:44:01 +1100

    Finished styling on base pages and components for mobile.

--- components/ProductDisplay.tsx
+++ components/ProductDisplay.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import { useRouter } from 'next/router'
 import { useState, useContext } from "react"
 import About from "./About"
 import CategoryPicker from "./CategoryPicker"
@@ -9,6 +10,7 @@ import styles from '../styles/pages/ProductDisplay.module.css'
 
 const ProductDisplay = ({product}: {product: Product}) => {
     const [ itemsToBuy, setItemsToBuy ] = useState<number>(1)
+    const router = useRouter()
     const value = useContext(AppContext)
     let { device }: {device: string} = value.state
     const splitting = [
@@ -17,75 +19,87 @@ const ProductDisplay = ({product}: {product: Product}) => {
     ]
     const alt: string = `Image of ${product.name}`
     return (
-        <main className="container">
-            <button className={styles.back_button}>Go Back</button> 
-            <section className={styles.product}>
-                <div className={styles.product_head}>
-                    <figure className={`${styles.product_head__image} ${product.new ? styles.product_head__image___gapped : ''}`}>
-                        <Image 
-                            src={product.image[device].slice(1)} 
-                            alt={alt} 
-                            layout='fill' 
-                        />
-                    </figure>
-                    <div className={styles.product_head_text}>
-                        {product.new && <p className={styles.product_head_text__new}>New Product</p>}
-                        <h1 className={styles.product_head_text__heading}>
-                            {splitting[0]}
-                            <span>{splitting[1]}</span>
-                        </h1>
-                        <p className={styles.product_head_text__body}>{product.description}</p>
-                        <p className={styles.product_head_text__price}>${product.price.toLocaleString('en')}</p>
-                        <div className={styles.product_head_text__buy_it}>
-                            <ul className={styles.buyIt}>
-                                <li className={styles.buyIt__math}>-</li>
-                                <li className={styles.buyIt__display}>{itemsToBuy}</li>
-                                <li>+</li>
-                            </ul>
-                            <button className="button-one">Add to cart</button>
+        <div className={styles.convenient}>
+            <button 
+                className={styles.back_button}
+                type='button'
+                onClick={() => router.back()}
+            >Go Back</button> 
+            <main className="container">
+                <section className={styles.product}>
+                    <div className={styles.product_head}>
+                        <figure className={`${styles.product_head__image} ${product.new ? styles.product_head__image___gapped : ''}`}>
+                            <Image 
+                                src={product.image[device].slice(1)} 
+                                alt={alt} 
+                                layout='fill' 
+                            />
+                        </figure>
+                        <div className={styles.product_head_text}>
+                            {product.new && <p className={styles.product_head_text__new}>New Product</p>}
+                            <h1 className={styles.product_head_text__heading}>
+                                {splitting[0]}
+                                <span>{splitting[1]}</span>
+                            </h1>
+                            <p className={styles.product_head_text__body}>{product.description}</p>
+                            <p className={styles.product_head_text__price}>${product.price.toLocaleString('en')}</p>
+                            <div className={styles.product_head_text__buy_it}>
+                                <ul className={styles.buyIt}>
+                                    <li 
+                                        onClick={() => itemsToBuy > 0 && setItemsToBuy(itemsToBuy - 1)} 
+                                        className={styles.buyIt__math}
+                                    >-</li>
+                                    <li className={styles.buyIt__display}>{itemsToBuy}</li>
+                                    <li 
+                                        onClick={() => setItemsToBuy(itemsToBuy + 1)} 
+                                        className={styles.buyIt__math}
+                                    >+</li>
+                                </ul>
+                                <button className="button-one">Add to cart</button>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div>
-                    <h2>Features</h2>
-                    <p>{product.features}</p>
-                </div>
-                <div>
-                    <h2>In the box</h2>
-                    <ul>
-                        {product.includes.map((e, i) => (
-                            <li key={i}><span>{e.quantity}x</span>{e.item}</li>
-                        ))}
-                    </ul>
-                </div>
-                <div>
-                    <figure>
-                        <Image 
-                            src={product.gallery.first[device].slice(1)} 
-                            alt={alt} 
-                            layout='fill' 
-                        />
-                    </figure>
-                    <figure>
-                        <Image 
-                            src={product.gallery.second[device].slice(1)} 
-                            alt={alt} 
-                            layout='fill' 
-                        />
-                    </figure>
-                    <figure>
-                        <Image 
-                            src={product.gallery.third.mobile.slice(1)} 
-                            alt={alt} 
-                            layout='fill' 
-                        />
-                    </figure>
-                </div>
-            </section>
-            <YouMayAlsoLike others={product.others} />
-            <CategoryPicker />
-            <About />
-        </main>
+                    <div className={styles.product_features}>
+                        <h2 className={styles.product_features__heading}>Features</h2>
+                        <p className={styles.product_features__body}>{product.features}</p>
+                    </div>
+                    <div className={styles.product_includes}>
+                        <h2 className={styles.product_includes__heading}>In the box</h2>
+                        <ul className={styles.product_includes_list}>
+                            {product.includes.map((e, i) => (
+                                <li className={styles.product_includes_list__item} key={i}><span>{e.quantity}x</span>{e.item}</li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div className={styles.product_images}>
+                        <figure className={`${styles.product_images__image} ${styles.product_images__image___one}`}>
+                            <Image 
+                                src={product.gallery.first[device].slice(1)} 
+                                alt={alt} 
+                                layout='fill' 
+                            />
+                        </figure>
+                        <figure className={`${styles.product_images__image} ${styles.product_images__image___two}`}>
+                            <Image 
+                                src={product.gallery.second[device].slice(1)} 
+                                alt={alt} 
+                                layout='fill' 
+                            />
+                        </figure>
+                        <figure className={`${styles.product_images__image} ${styles.product_images__image___three}`}>
+                            <Image 
+                                src={product.gallery.third.mobile.slice(1)} 
+                                alt={alt} 
+                                layout='fill' 
+                            />
+                        </figure>
+                    </div>
+                </section>
+                <YouMayAlsoLike others={product.others} />
+                <CategoryPicker />
+                <About />
+            </main>
+        </div>
     )
 }
 
--- components/YouMayAlsoLike.tsx
+++ components/YouMayAlsoLike.tsx
@@ -1,28 +1,33 @@
 import { Others } from "../lib/types"
 import Image from "next/image"
 import Link from "next/link"
+import styles from '../styles/components/YouMayAlsoLike.module.css'
+
 
 const YouMayAlsoLike = ({others}: {others: Others}) => {
     const device = 'mobile'
     
     return ( 
-        <div>
-            <h2>You may also like</h2>
-            <ul>
+        <div className={styles.others}>
+            <h2 className={styles.others__heading}>You may also like</h2>
+            <ul className={styles.others_list}>
                 {
                     others.map((e, i) => {
                         let category: string = e.slug.slice(e.slug.lastIndexOf('-') + 1) 
                         if(!category.endsWith('s')) category += 's'
                         return (
-                            <li key={i}>
-                                <figure>
+                            <li className={styles.others_list_item} key={i}>
+                                <figure className={styles.others_list_item__image}>
                                     <Image 
                                         src={e.image[device].slice(1)}
                                         alt={`Image of ${e.name}`}
                                         layout='fill'
                                     />
                                 </figure>
-                                <Link href={`/${category}/${e.slug}`}><a>See Product</a></Link>
+                                <h3 className={styles.others_list_item__heading}>{e.name}</h3>
+                                <Link href={`/${category}/${e.slug}`}>
+                                    <a className={`button-one ${styles.others_list_item__link}`}>See Product</a>
+                                </Link>
                             </li>
                         )
                     }
